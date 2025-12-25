3fee2902713e4cd8a65ce1a0591c81a8148f4530
Manish Sharma <42198987+sharma2464@users.noreply.github.com>
2025-12-25 17:01:39 +0530

    Change video codec from libx265 to libx264

--- engines/videotoolbox.py
+++ engines/videotoolbox.py
@@ -3,25 +3,17 @@ from pathlib import Path
 
 def encode_ffmpeg(input_path: Path, output_path: Path, quality: int):
     cmd = [
-        "ffmpeg",
-        "-y",
+        "ffmpeg", "-y",
         "-i", str(input_path),
-
         "-map", "0:v:0",
         "-map", "0:a?",
-        "-map_metadata", "0",
-
-        "-c:v", "libx265",
+        "-c:v", "libx264",
         "-crf", str(quality),
-        "-preset", "medium",
-
+        "-preset", "veryfast",
         "-c:a", "aac",
         "-b:a", "128k",
-
         "-movflags", "+faststart",
         str(output_path)
     ]
-
-    print(f"🎞 Encoding: {input_path.name}")
 
     subprocess.run(cmd, check=True)
